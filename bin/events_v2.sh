fecf42c45768dc61b2c8c01dd88c82bf0c9b7402
Logiqx <software@mikeg.me.uk>
2022-08-17 23:18:58 +0100

    Add scripts for V2 API

---
+++ bin/events_v2.sh
@@ -0,0 +1,9 @@
+# Project Env
+. $(dirname $0)/env.sh
+
+# Run the script to download event sessions and rankings, then do the checks
+run_py_script download_events_v2.py
+run_py_script check_events_v2.py
+
+# View differences
+git diff docs/events/README.md
